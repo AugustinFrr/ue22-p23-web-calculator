93093fe742b18422f9deb373fb24c524c91fbbf6
Augustin Ferrey <augustin.ferrey@etu.minesparis.psl.eu>
2024-03-21 22:55:24 +0100

    commit des modifications jusqu'aux opérations

--- subject/script.js
+++ subject/script.js
@@ -3,3 +3,20 @@
 
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
+
+window.addEventListener("load", () => {
+    let buttons = document.querySelectorAll("button");
+    let screen = document.getElementsByClassName("calculator__display") [0]; // écran de la calculatrice (il n'y a qu'un div dans cette classe)
+
+    for (let button of buttons) {
+        button.addEventListener("click", (evenement) => {
+            const bouton = evenement.target;
+            if (bouton.getAttribute("data-action") === "clear") {
+                screen.textContent = "0";
+            } else {
+            screen.textContent = button.textContent; // affiche la valeur du outn clické
+            }
+        });
+    };
+});
+
